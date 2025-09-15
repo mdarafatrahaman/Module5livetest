d3532cd3a09a4609bbc64ee524f468ba86cc3832
Arafat <mdarafatrahaman.it@gmail.com>
2025-09-15 23:19:42 +0600

    Module5livetest

--- bin/Module5livetest.dart
+++ bin/Module5livetest.dart
@@ -1,3 +1,59 @@
 main(){
-  print('hello world');
+
+  book Book = book('English Book', 'Tomas', 150);
+
+}
+class book{
+    String ? title;
+    String ? author;
+    int ? price;
+
+    //constractor
+  book(this.title, this.author, this.price){
+    print('title of book');
+    print('Author of book');
+    print('price of book');
+  }
+
+    discountedPrice(){
+    print('calculate the price');
+    }
+}
+
+class Employee {
+
+  String name ='Arif';
+  int salary = 10000;
+
+  Employee(this.name, this.salary);
+}
+
+//subclass 1: Manager
+
+  class Manager extends Employee{
+  String ? department;
+
+  Manager(super.name){
+    print('object');
+  }
+}
+
+//subclass 2: Manager
+
+class Developer extends Employee {
+  String ? programmingLanguage;
+}
+
+abstract class Appliance {
+
+  truOn() {
+    print('Fan is now running');
+}
+  turnOff(){
+    print('Fan is Stop');
+  }
+}
+
+class Fan extends Appliance{
+
 }
